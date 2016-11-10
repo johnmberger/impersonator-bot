a5df52778481c21b159efe12b755a690d7b59c57
johnmberger <johnmartinberger@gmail.com>
2016-11-10 09:51:02 -0700

    making changes

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import praw
 
 # reddit really wants you to use a unique user agent string.
 # see https://github.com/reddit/reddit/wiki/API#rules
-r = praw.Reddit(user_agent='redditbot 0.1 by /u/')
+r = praw.Reddit(user_agent='reddit impersonator 0.1')
 # login isn't strictly needed here since we're not
 # posting, commenting, etc.
 # you'll need to set the REDDIT_USER and REDDIT_PASS
@@ -13,6 +13,6 @@ r = praw.Reddit(user_agent='redditbot 0.1 by /u/')
 r.login(os.environ['REDDIT_USER'], os.environ['REDDIT_PASS'])
 
 while True:
-    for submission in r.get_subreddit('learnpython').get_hot(limit=5):
+    for submission in r.get_subreddit('ImpersonatorBot').get_hot(limit=5):
         print(submission)
     time.sleep(15)
