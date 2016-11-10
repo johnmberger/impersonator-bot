2819fa4c67b3390deb0dc8ec76ea125bf9d966cd
johnmberger <johnmartinberger@gmail.com>
2016-11-10 09:44:33 -0700

    deploying to heroku

---
+++ bot.py
@@ -0,0 +1,18 @@
+import os
+import time
+
+import praw
+
+# reddit really wants you to use a unique user agent string.
+# see https://github.com/reddit/reddit/wiki/API#rules
+r = praw.Reddit(user_agent='ImpersonatorBot 0.1')
+# login isn't strictly needed here since we're not
+# posting, commenting, etc.
+# you'll need to set the REDDIT_USER and REDDIT_PASS
+# environment variables before you run this bot
+r.login(os.environ['ImpersonatorBot'], os.environ['chilldude'])
+
+while True:
+    for submission in r.get_subreddit('ImpersonatorBot').get_hot(limit=5):
+        print(submission)
+    time.sleep(15)
